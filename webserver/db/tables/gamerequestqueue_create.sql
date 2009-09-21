308acabb00e163049e8688eabd11279c339913b1
Hugh Perkins <hughperkins@gmail.com>
2009-09-21 20:53:06 +0800

    finished purge following restructuring, started to create sql scripts for tables

---
+++ webserver/db/tables/gamerequestqueue_create.sql
@@ -0,0 +1,13 @@
+create table gamerequestqueue (
+   int gamerequestid,
+   varchar(255) ai0name,
+   varchar(255) ai0version,
+   varchar(255) ai1name,
+   varchar(255) ai1version
+   varchar(255) map,
+   varchar(255) maphash,
+   varchar(255) mod,
+   varchar(255) modhash
+   boolean cheatingallowed
+);
+
