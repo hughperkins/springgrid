d0e08f9d678f2d4755beae1272f696c5e1d844ef
Hugh Perkins <hughperkins@gmail.com>
2009-09-22 00:10:05 +0800

    added matchresults tables

---
+++ webserver/db/tables/matchresults_create.sql
@@ -0,0 +1,14 @@
+create table matchresults (
+   matchrequestid integer not null,
+   ai0name varchar(255) not null,
+   ai0version varchar(255) not null,
+   ai1name varchar(255) not null,
+   ai1version varchar(255) not null,
+   mapname varchar(255) not null,
+   maphash varchar(255) not null,
+   modname varchar(255) not null,
+   modhash varchar(255) not null,
+   cheatingallowed varchar(255) not null default 'yes',
+   matchresult varchar(255) not null
+);
+
