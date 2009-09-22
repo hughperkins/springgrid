935570cb4146635908c3f98388b11d0d299bd5ea
Hugh Perkins <hughperkins@gmail.com>
2009-09-22 10:20:26 +0800

    added bash scripts to create/drop the tables.

---
+++ webserver/db/tables/dropall.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+user=$1
+password=$2
+dbname=$3
+dbhost=$4
+
+if [[ x$dbhost == x ]]; then {
+   echo Usage:
+   echo $0 [user] [password] [dbname] [hostname]
+   exit 1
+} fi
+
+for sqlfile in $(ls *_drop.sql); do {
+      # echo $sqlfile
+      mysql -u $user --password=$password --host=$dbhost $dbname <$sqlfile
+} done;
+
+
