adcd05d977dc8758a462e2eca584a11a216d193c
Hugh Perkins <hughperkins@gmail.com>
2009-09-22 11:00:46 +0800

    added placholder for calcenginegetrequest.py

---
+++ webserver/website/calcenginegetrequest.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+# Copyright Hugh Perkins 2009
+# hughperkins@gmail.com http://manageddreams.com
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+#  more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program in the file licence.txt; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-
+# 1307 USA
+# You can find the licence also on the web at:
+# http://www.opensource.org/licenses/gpl-license.php
+#
+# ======================================================================================
+#
+
+# job of this is to deliver a request from the queue to a calcengine
+# and somehow mark that that request is undergoing processing
+
+# inputs:
+# - name of calcengine
+# - calcengine sharedsecret
+
+# as far as maps and mods that the calcengine can use:
+# - this info will be obtained from the database
+# - either the calcengine can signal that it will download things as it goes
+# - or the database will contain a list of maps and mods that each engine currently has
+# - for now, we'll just store a list of maps and mods supported by each engine in the database, and deal with the issue of downloading later
+#
+
+#
+# so... we're going to have to:
+# - connect to the database
+# - validate the engine's shared secret
+# - get an item from the queue
+# - check it's compatible with the engine
+# - mark the request as undergoing processing, with a date/time stamp
+# - create the appropriate xml to return the request to the calcengine
+
+
+
