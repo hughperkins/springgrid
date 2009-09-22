f57b1253da01b746f19ce58a49ff0f9f01787754
Hugh Perkins <hughperkins@gmail.com>
2009-09-22 18:54:14 +0800

    added name to calcengines table.

--- webserver/db/tables/calcengines_create.sql
+++ webserver/db/tables/calcengines_create.sql
@@ -21,7 +21,8 @@
 
 create table calcengines (
    calcengine_id integer not null,
-   calcengine_contactname varchar(255) not null,
+   calcengine_name varchar(255) not null,
+   calcengine_contactname varchar(255) not null default '',
    calcengine_sharedsecret varchar(255) not null
 );
 
