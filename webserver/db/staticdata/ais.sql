f7c2f716457b114eac1b4b9a8ddf452fe3fea8f0
Hugh Perkins <hughperkins@gmail.com>
2009-09-24 23:05:45 +0800

    tweaked version of HughAI ;-)

--- webserver/db/staticdata/ais.sql
+++ webserver/db/staticdata/ais.sql
@@ -1,5 +1,5 @@
 insert into ais ( ai_name, ai_version, ai_downloadurl, ai_owneraccount_id)
-   values ( 'HughAI', '1.0', 'http://manageddreams.com/hughai/HughAI20090921b.tar.bz2', 1 );
+   values ( 'HughAI', '0.1', 'http://manageddreams.com/hughai/HughAI20090921b.tar.bz2', 1 );
 insert into ais ( ai_name, ai_version, ai_downloadurl, ai_owneraccount_id ) 
    values ( 'E323AI', '2.12.0', 'http://cloud.github.com/downloads/Error323/E323AI/E323AI-v2.12.0-Ultralisk.tgz', 1 );
 
