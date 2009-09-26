a1fed5f60d345785a2fd0135352cbaa1c230a864
Hugh Perkins <hughperkins@gmail.com>
2009-09-26 23:56:19 +0800

    improved the menu css

--- webserver/db/staticdata/accounts.sql
+++ webserver/db/staticdata/accounts.sql
@@ -1,5 +1,3 @@
-# test data obviously...
-
-insert into accounts (username, userfullname, passwordsalt, passwordhash) values ('admin', 'admin','r3r3rnbkl35hl5jgl3rlaekgt4t',md5(concat('admin','r3r3rnbkl35hl5jgl3rlaekgt4t') ) );
-insert into accounts (username, userfullname, passwordsalt, passwordhash ) values ('guest', 'guest','aerg4g85htgh2fg5ghegrg4g24g', md5(concat('guest','aerg4g85htgh2fg5ghegrg4g24g')));
+insert into accounts (username, userfullname, passwordsalt, passwordhash) values ('admin', 'admin','r3r3rnbkrgrgWRwrgwrgwrHthwrhwtjwrhjwtjwrhwrjWRjwrhergrwhl35hl5jgl3rlaekgt4t',md5(concat('admin','r3r3rnbkrgrgWRwrgwrgwrHthwrhwtjwrhjwtjwrhwrjWRjwrhergrwhl35hl5jgl3rlaekgt4t') ) );
+insert into accounts (username, userfullname, passwordsalt, passwordhash ) values ('guest', 'guest','aerg4g85htgh2fg5g8ighWRG@$g2g2G@$G@$g24g24ggwvwe5ghegrg4g24g', md5(concat('guest','aerg4g85htgh2fg5g8ighWRG@$g2g2G@$G@$g24g24ggwvwe5ghegrg4g24g')));
 
