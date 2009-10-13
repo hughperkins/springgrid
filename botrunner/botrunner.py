c4573dc319f7352cd4b997d7387f9170c804e408
hoijui <hoijui.quaero@gmail.com>
2009-10-13 20:28:45 +0200

    Revert "give user friendly message when registering parts failed"
    This reverts commit d2c4594a72c61d5023563caaa682137849343825.
    
    The exception gives a more detailed error message when not catched

--- botrunner/botrunner.py
+++ botrunner/botrunner.py
@@ -384,13 +384,9 @@ def registermaps():
       #print unitsync.GetMapArchiveName(1)
       archivechecksum = unitsync.GetArchiveChecksum( archivename )
       requestparams = urllib.urlencode({'mapname': mapname, 'maparchivechecksum': archivechecksum, 'botrunnername': config.botrunnername, 'sharedsecret': config.sharedsecret })
-      regMapUrl = config.websiteurl + "/botrunner_registersupportedmap.py"
-      try:
-         serverrequesthandle = urllib.urlopen( regMapUrl, requestparams )
-         serverrequestarray = serverrequesthandle.readlines()
-         print "\n".join( serverrequestarray )
-      except:
-         print "\n" + "Failed registering map " + mapname + " on " + regMapUrl
+      serverrequesthandle = urllib.urlopen( config.websiteurl + "/botrunner_registersupportedmap.py", requestparams )
+      serverrequestarray = serverrequesthandle.readlines()
+      print "\n".join( serverrequestarray )
 
 def registermods():
    for i in xrange( unitsync.GetPrimaryModCount() ):
@@ -400,13 +396,9 @@ def registermods():
       modarchive = unitsync.GetPrimaryModArchive(0)
       modarchivechecksum = unitsync.GetArchiveChecksum( modarchive )
       requestparams = urllib.urlencode({'modname': modname, 'modarchivechecksum': modarchivechecksum, 'botrunnername': config.botrunnername, 'sharedsecret': config.sharedsecret })
-      regModUrl = config.websiteurl + "/botrunner_registersupportedmod.py"
-      try:
-         serverrequesthandle = urllib.urlopen( regModUrl, requestparams )
-         serverrequestarray = serverrequesthandle.readlines()
-         print "\n".join( serverrequestarray )
-      except:
-         print "\n" + "Failed registering mod " + modname + " on " + regModUrl
+      serverrequesthandle = urllib.urlopen( config.websiteurl + "/botrunner_registersupportedmod.py", requestparams )
+      serverrequestarray = serverrequesthandle.readlines()
+      print "\n".join( serverrequestarray )
 
 def registerais():
    for i in xrange( unitsync.GetSkirmishAICount() ):
@@ -417,16 +409,12 @@ def registerais():
             shortname = unitsync.GetInfoValue(j)
          if unitsync.GetInfoKey(j) == "version":
             version = unitsync.GetInfoValue(j)
-
+         
       if shortname != '' and version != '':
          print "registering ai " + shortname + " version " + version + " ..."
       requestparams = urllib.urlencode({'ainame': shortname, 'aiversion': version, 'botrunnername': config.botrunnername, 'sharedsecret': config.sharedsecret })
-      regAIUrl = config.websiteurl + "/botrunner_registersupportedai.py"
-      try:
-         serverrequesthandle = urllib.urlopen( regAIUrl, requestparams )
-         serverrequestarray = serverrequesthandle.readlines()
-      except:
-         print "\n" + "Failed registering AI " + shortname + " " + version + " on " + regAIUrl
+      serverrequesthandle = urllib.urlopen( config.websiteurl + "/botrunner_registersupportedai.py", requestparams )
+      serverrequestarray = serverrequesthandle.readlines()
 
 def go():
    global config, unitsync, writabledatadirectory
