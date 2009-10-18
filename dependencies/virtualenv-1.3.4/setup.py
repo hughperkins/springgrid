65c44669dc0b8faa74c1b2eaa9ff734069e6dbe1
Hugh Perkins <hughperkins@gmail.com>
2009-10-18 11:16:02 +0800

    Added virtualenv and sqlalchemy to dependencies directory.

---
+++ dependencies/virtualenv-1.3.4/setup.py
@@ -0,0 +1,41 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+    print 'Note: without Setuptools installed you will have to use "python -m virtualenv ENV"'
+import sys, os
+
+version = '1.3.4'
+
+try:
+    f = open(os.path.join(os.path.dirname(__file__), 'docs', 'index.txt'))
+    long_description = f.read().strip()
+    f.close()
+except IOError:
+    long_description = None
+
+setup(name='virtualenv',
+      version=version,
+      description="Virtual Python Environment builder",
+      long_description=long_description,
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+      ],
+      keywords='setuptools deployment installation distutils',
+      author='Ian Bicking',
+      author_email='ianb@colorstudy.com',
+      url='http://pypi.python.org/pypi/virtualenv',
+      license='MIT',
+      py_modules=['virtualenv'],
+      ## Hacks to get the package data installed:
+      packages=[''],
+      package_dir={'': '.'},
+      package_data={'': ['support-files/*-py%s.egg' % sys.version[:3]]},
+      zip_safe=False,
+      entry_points="""
+      [console_scripts]
+      virtualenv = virtualenv:main
+      """,
+      )
