f4331e9e4140ca056e7602292489ce165417e097
Hugh Perkins <hughperkins@gmail.com>
2009-10-20 14:52:35 +0800

    migrated viewaccounts to jinja

--- webserver/website/viewaccounts.py
+++ webserver/website/viewaccounts.py
@@ -31,51 +31,19 @@ sqlalchemysetup.setup()
 
 loginhelper.processCookie()
 
-menu.printPageTop()
-
 def go():
    if loginhelper.gusername == '':
-      print "Please login first before using this page."
+      jinjahelper.message( "Please login first before using this page." )
       return
 
    accounts = sqlalchemysetup.session.query( Account )
 
-   print "<h3>AILadder - Account List</h3>" \
-   "<table border='1' padding='3'>" \
-   "<tr class='tablehead'><td>username</td><td>User full name</td><td>Delete</td></tr>"
+   showform = roles.isInRole(roles.accountadmin)
 
-   for account in accounts:
-      print "<tr>"
-      print "<td><a href='viewaccount.py?username=" + account.username + "'>" + account.username + "</td><td>" + account.userfullname + "</a></td>"
-      if not roles.isInRole2( account.username, roles.accountadmin ):
-         print "<td><a href='removeaccount.py?username=" + account.username + "'>Delete account</a></td>"
-      else:
-         print "<td>(Drop accountadmin role before deleting account)</td>"
-      print "</tr>"
-
-   print "</table>"
-
-   if roles.isInRole(roles.accountadmin):
-
-      print "<p />"
-      print "<hr />"
-      print "<p />"
-
-      print "<h4>Create new account:</h4>"
-      print "<form action='addaccount.py' method='post'>" \
-      "<table border='1' padding='3'>" \
-      "<tr><td>Username</td><td><input name='username'</td></tr>" \
-      "<tr><td>User full name</td><td><input name='userfullname'</td></tr>" \
-      "<tr><td>User email address</td><td><input name='useremailaddress'</td></tr>" \
-      "<tr><td>User password</td><td><input type='password' name='userpassword'</td></tr>" \
-      "<tr><td></td><td><input type='submit' value='Add' /></td></tr>" \
-      "</table>" \
-      "</form>"
-
+   jinjahelper.rendertemplate('viewaccounts.html', accounts = accounts, showform = showform, roles = roles )
 
 go()
 
 sqlalchemysetup.close()
 
-menu.printPageBottom()
 
