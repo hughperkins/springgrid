3b32635b16a6412304a7400b6e3850358fdb918f
Hugh Perkins <hughperkins@gmail.com>
2009-11-22 02:01:37 +0800

    created bootstrap_cloudbotrunner.py

---
+++ webserver/website/bootstrap_cloudbotrunner.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+# generates a bash script that, when run, will download what we needd, and launch
+# the botrunner :-O
+#
+# This script assumes it will be run on disposable ec2 cloud nodes, and does not
+# take any steps to protect the os from being compromised
+
+from core import *
+from utils import *
+
+jinjahelper.rendertemplate('bootstrap_cloudbotrunner.html')
+
